9716a2b888dc603182929b839fd7afad7d38f53c
m-spiridon <spiridonov.max.w@gmail.com>
2021-02-02 17:28:27 +0200

    tried to add shadow and popup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Shrink navbar on scroll and Progress-bar
+//Progress-bar
 
 window.onscroll = scroll;
 function scroll() {
@@ -6,14 +6,31 @@ function scroll() {
   const pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   const progressBarWidth = (distanceFromTop / pageHeight) * 100;
   document.querySelector('.progress-bar').style.width = progressBarWidth + '%';
-}
+};
 
 //Close menu on click outside
 
-var close = document.getElementsByClassName("close")[0];
+document.querySelector('.send-button').addEventListener('click', function() {
+  document.querySelector('.menu-shadow').style.visibility = 'visible';
+  document.querySelector('.menu-shadow').style.opacity = '1';
+  document.querySelector('.menu-shadow').style.zIndex = '3';
+  document.querySelector('.popup').style.visibility = 'visible';
+  document.querySelector('.popup').style.opacity = '1';
+});
 
-window.onclick = function(e) {
-  if (e.target == close) {
-    document.getElementById("burger").checked = false;
-  }
-}
+document.querySelector('.menu-shadow').addEventListener('click', function() {
+  document.querySelector('.menu-shadow').style.visibility = 'hidden';
+  document.querySelector('.menu-shadow').style.opacity = '0';
+  document.querySelector('.menu-shadow').style.zIndex = '0';
+  document.querySelector('.popup').style.visibility = 'hidden';
+  document.querySelector('.popup').style.opacity = '0';
+});
+
+document.querySelector('.popup-close').addEventListener('click', function() {
+  document.querySelector('.menu-shadow').style.visibility = 'hidden';
+  document.querySelector('.menu-shadow').style.opacity = '0';
+  document.querySelector('.menu-shadow').style.zIndex = '0';
+  document.querySelector('.popup').style.visibility = 'hidden';
+  document.querySelector('.popup').style.opacity = '0';
+});
+
